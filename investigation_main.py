586b02b41af018b2fbc5ac22e33b3ab7cbbfb0bf
Sangmin Jeon <wjstkdals409@naver.com>
2019-06-26 21:57:32 +0900

    Investigation BOT prot.

--- investigation_main.py
+++ investigation_main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 TOKEN = 'NTkzMjM1MzA1MDM3OTU1MDky.XRLK9g.Ge8G81f60d265MV2yiyzxA9io_s'
 
@@ -6,14 +7,39 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('!사용법'):
-        embed = discord.Embed(title="수색요정사용법", description="인형 제조시간 : hhmm으로 입력 예시)4시간 4분->0404\n장비/인형제조시간 : .hhmm으로 입력 예시)0시간 52분->.0052 5시간 5분->.0505", color = 0x00ff00)
+        embed = discord.Embed(title="수색요정사용법", description="인형 제조시간 : ,hhmm으로 입력 예시)4시간 4분->,0404\n장비/요정제조시간 : .hhmm으로 입력 예시)0시간 52분->.0052 5시간 5분->.0505", color = 0x00ff00)
         msg = '{0.author.mention}'.format(message)
         await client.send_message(message.channel, embed=embed)
+    elif message.content.startswith(','):
+        infile = open("doll.txt", "r")
+        t = infile.read()
+        time = str(message.content[1:])
+        loc = int(t.find(time))
+        if loc == -1 :
+            sen = " 처음보는 시간인데요?"
+            msg = ('{0.author.mention}' + sen).format(message)
+            await client.send_message(message.channel, msg)
+        else :
+            sen = t[loc+4:t.find("\n",loc)]
+            msg = ('{0.author.mention}' + sen).format(message)
+            await client.send_message(message.channel, msg)
+    elif message.content.startswith('.'):
+        infile = open("equip.txt", "r")
+        t = infile.read()
+        time = str(message.content[1:])
+        loc = int(t.find(time))
+        if loc == -1 :
+            sen = " 처음보는 시간인데요?"
+            msg = ('{0.author.mention}' + sen).format(message)
+            await client.send_message(message.channel, msg)
+        else :
+            sen = t[loc+4:t.find("\n",loc)]
+            msg = ('{0.author.mention}' + sen).format(message)
+            await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
